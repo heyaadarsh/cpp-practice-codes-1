0b41208f79cea2e3336ecc3dd492eed221c6f9c2
A2v10 <hey.a2v10@gmail.com>
2022-03-13 14:57:38 +0530

    loops

--- loops.cpp
+++ loops.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
+   int i = 0;
+   cout<<"Table of 6:"<<endl;
+   while (i<60)
+   {
+      i+=6;
+      cout<<i<<endl;
+   }
+
+   cout<<"Second Approach"<<endl;
+
+   for (int i = 1; i <= 60; i++)
+   {
+      /* code */
+      if (i%6==0)
+      {
+         cout<<i<<endl;
+      }
+      
+   }
+   
+   
 
    return 0;
 }
